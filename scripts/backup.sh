feaa717465d4b7e286d77128506f97272e45eb73
SeanDuffie <sean.duffie@tamu.edu>
2023-02-27 10:22:58 -0600

    Switch to hourly default add initial revert_backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,31 +1,30 @@
 #usr/bin/bash
 
-# Backs up the Minecraft server once a day, and deletes any backup older than 2 days
+# Backs up the Minecraft server once an hour, and deletes any backup older than 3 hours
 
 # Notifies the server of a backup
-sudo screen -S minecraft_server -x stuff "say Daily Backup starting. Things won't save for a moment... $(printf '\r')"
-sudo screen -S minecraft_server -x stuff "save-off $(printf '\r')"
-sudo screen -S minecraft_server -x stuff "save-all $(printf '\r')"
+screen -S minecraft_server -X stuff "say Hourly Backup starting. Things won't save for a moment... $(printf '\r')"
+screen -S minecraft_server -X stuff "save-off $(printf '\r')"
+screen -S minecraft_server -X stuff "save-all $(printf '\r')"
 
 # Moves to the directory
-cd /home/ubuntu/FTP/Minecraft_Backups/Daily
+cd /home/ubuntu/FTP/Minecraft_Backups/Hourly
 
 # Remove older directory
-sudo rm -f minecraft-day2.tar.gz
+sudo rm -f minecraft-hour3.tar.gz
 
 # Shift existing directories
-# sudo mv minecraft-day3.tar.gz minecraft-day4.tar.gz
-# sudo mv minecraft-day2.tar.gz minecraft-day3.tar.gz
-# sudo mv minecraft-day1.tar.gz minecraft-day2.tar.gz
-sudo mv minecraft-day1.tar.gz minecraft-day2.tar.gz
+mv minecraft-hour2.tar.gz minecraft-hour3.tar.gz
+mv minecraft-hour1.tar.gz minecraft-hour2.tar.gz
+mv minecraft-hour0.tar.gz minecraft-hour1.tar.gz
 
-# Creates a new backup for the most recent day
+# Creates a new backup for the most recent hour
 cd /home/ubuntu/FTP/
-sudo tar -zcvf Minecraft_Backups/Daily/minecraft-day1.tar.gz Minecraft_Server # --exclude
+tar -zcvf Minecraft_Backups/Hourly/minecraft-hour1.tar.gz Minecraft_Server # --exclude
 # ''
 
 # Notifies the server of Backup Completion
-sudo screen -S minecraft_server -x stuff "save-on $(printf '\r')"
-sudo screen -S minecraft_server -x stuff "say Daily Backup complete! The world is saving again. $(printf '\r')"
+screen -S minecraft_server -X stuff "save-on $(printf '\r')"
+screen -S minecraft_server -X stuff "say Hourly Backup complete! The world is saving again. $(printf '\r')"
 
 exit 0
