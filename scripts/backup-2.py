694bbff2fdd0af7b7b7537f3728c44d13279a792
SeanDuffie <duffiesean@gmail.com>
2024-04-17 17:08:40 -0400

    Added the start of my changes to xangma's scripts

--- scripts/backup-2.py
+++ scripts/backup-2.py
@@ -1,17 +1,37 @@
+""" @file backup.py
+    @author Sean Duffie
+    @brief 
+"""
+import logging
 import os
 import subprocess
-import time
+import sys
 import tarfile
+import time
 from datetime import datetime
 from threading import Timer
-import logging
-import sys
+from tkinter import filedialog
 
-minecraft_dir = "F:/home/xangma/MCSERVER/survival"
-backup_dir = "Z:/MCSERVER_WORLD1"
-executable = 'java -Xmx2048m -XX:+UseConcMarkSweepGC -jar "%s/spigot-1.12.2.jar"' %minecraft_dir
+### CONSTANTS SECTION ###
+min_ram = 8
+hourly_cap = 6
+daily_cap = 3
+
+### PATH SECTION ###
+default_dir = os.path.dirname(__file__)
+minecraft_dir = filedialog.askdirectory(
+                    title="Select Server Directory",
+                    initialdir=f"{default_dir}/world/"
+                )
+backup_dir = filedialog.askdirectory(
+                    title="Select Backup Directory",
+                    initialdir=f"{default_dir}/backups/"
+                )
+executable = f'java -Xmx{min_ram * 1024}m -XX:+UseConcMarkSweepGC -jar "{minecraft_dir}/spigot-1.12.2.jar"'
+# TODO: Is this needed?
 exclude_file = "plugins/dynmap"
 
+### LOGGING SECTION ###
 logname = minecraft_dir + '/' + 'MCSERVER.log'
 file_handler = logging.FileHandler(filename=logname)
 stdout_handler = logging.StreamHandler(sys.stdout)
@@ -24,14 +44,24 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger('MCLOG')
-logging.info('Logname: %s' %logname)
+logging.info(f"Logname: {logname}")
 
-def server_command(cmd):
-    logging.info('Writing server command')
+def server_command(cmd: str):
+    """ Sends a string from the local python script to the server shell process
+
+    Args:
+        cmd (str): The command string to be sent to the server
+    """
+    logging.info("Writing server command: %s", cmd)
     process.stdin.write(str.encode('%s\n' %cmd)) #just write the command to the input stream
     process.stdin.flush()
 
 def server_start():
+    """ Start the server in a subprocess and return a link to it.
+
+    Returns:
+        subprocess.Popen: The subprocess interface to the executable.
+    """
     os.chdir(minecraft_dir)
     logging.info('Starting server')
     process = subprocess.Popen(executable, stdin=subprocess.PIPE)
@@ -39,6 +69,14 @@ def server_start():
     return process
 
 def filter_function(tarinfo):
+    """ TODO: What does this function do?
+
+    Args:
+        tarinfo (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
      if tarinfo.name != exclude_file:
           logging.info(tarinfo.name,"ADDED")
           return tarinfo
@@ -57,13 +95,17 @@ def backup():
     os.chdir(backup_dir)
     logging.info('Deleting last file')
     try:
-        os.remove("%s\\minecraft-hour24.tar.gz"%backup_dir)
-    except:
+        os.remove(f"{backup_dir}/minecraft-hour24.tar.gz")
+    except OSError:
         pass
     logging.info('Renaming old files')
+    # FIXME: This is a terrible iteration system, I bet I can do better with simple datetime and timeintervals
     for i in range(24,0,-1):
         try:
-            os.rename("%s\\minecraft-hour%s.tar.gz"%(backup_dir,i-1), "%s\\minecraft-hour%s.tar.gz" %(backup_dir,i))
+            os.rename(
+                f"{backup_dir}\\minecraft-hour{i-1}.tar.gz",
+                f"{backup_dir}\\minecraft-hour{i}.tar.gz"
+            )
         except:
             pass
     make_tarfile("%s\\minecraft-hour0.tar.gz"%backup_dir, minecraft_dir+"/")
@@ -79,6 +121,11 @@ def backup():
         os.popen('TASKKILL /PID '+str(process.pid)+' /F')
 
 def next_backuptime():
+    """ TODO: Add better docs
+
+    Returns:
+        int: seconds left before next backup
+    """
     logging.info('Calculating next time')
     x=datetime.today()
     if x.hour != 23:
@@ -100,14 +147,17 @@ def next_backuptime():
     secs=delta_t.seconds+1
     return secs
 
-process=server_start() # START SERVER
-time.sleep(60) # WAIT FOR IT TO START
 
-logging.info('Starting backup timer')
-try:
-    t = Timer(next_backuptime(), backup) # FIND NEXT HOURLY MARK
-    t.start() # START BACKUP FOR THEN
-    logging.info('Timer started')
-except:
-    logging.info('',exc_info=True)
-    os.popen('TASKKILL /PID '+str(process.pid)+' /F')
+
+if __name__ == "__main__":
+    process=server_start() # START SERVER
+    time.sleep(60) # WAIT FOR IT TO START
+
+    logging.info('Starting backup timer')
+    try:
+        t = Timer(next_backuptime(), backup) # FIND NEXT HOURLY MARK
+        t.start() # START BACKUP FOR THEN
+        logging.info('Timer started')
+    except:
+        logging.info('',exc_info=True)
+        os.popen('TASKKILL /PID '+str(process.pid)+' /F')
