07d4c7951d0e450b4d422eefbe8fb05ae081ae3e
SeanDuffie <duffiesean@gmail.com>
2024-04-17 16:33:13 -0400

    Set up initial barebones backup script Added placeholder for config file Added placeholder for manifest file Added test data to zip up

--- scripts/backup.py
+++ scripts/backup.py
@@ -1,13 +1,17 @@
 """ @file backup.py
     @author Sean Duffie
-    @
+    @brief script to backup files on a schedule
 """
 import datetime
 import os
 import shutil
+from tkinter import filedialog
 
 # Get the current working directory
-cwd = os.getcwd()
+cwd = filedialog.askdirectory(
+                title="Select Directory to be zipped",
+                initialdir=os.path.dirname(__file__)
+            )
 
 # Get the path to the Minecraft server world
 server_world_path = os.path.join(cwd, 'world')
@@ -21,7 +25,7 @@ if not os.path.exists(backup_dir_path):
 date_time = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
 # Create a backup file name
-backup_file_name = f'minecraft_server_backup_{date_time}.zip'
+backup_file_name = f'minecraft_server_backup_{date_time}'
 
 # Create a zip archive of the Minecraft server world
 shutil.make_archive(os.path.join(backup_dir_path, backup_file_name), 'zip', server_world_path)
