2e1af7bc44ed60f728e6e693fee55c98e57bc841
SeanDuffie <duffiesean@gmail.com>
2024-04-23 14:40:15 -0400

    swap logging for logger object

--- scripts/run-server.py
+++ scripts/run-server.py
@@ -3,17 +3,16 @@
     @brief 
 """
 import datetime
-import signal
 import logging
 import os
+import shutil
+import signal
 import subprocess
 import sys
-import time
-import threading
-from tkinter import filedialog
-
 import tarfile
-import shutil
+import threading
+import time
+from tkinter import filedialog
 from typing import Literal
 
 ### CONSTANTS SECTION ###
@@ -24,13 +23,13 @@ server_name = "Server"
 
 ### PATH SECTION ###
 default_path = os.path.dirname(__file__)
-server_path = default_path + "/Server/"
+server_path = f"{default_path}/{server_name}/"
 while server_path is None:
     server_path = filedialog.askdirectory(
                         title="Select Server Directory",
                         initialdir=f"{default_path}/"
                     )
-backup_path = server_path + "/backups/"
+backup_path = server_path + "../backups/"
 while backup_path is None:
     backup_path = filedialog.askdirectory(
                         title="Select Backup Directory",
@@ -57,7 +56,7 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger('MCLOG')
-logging.info("Logname: %s", logname)
+logger.info("Logname: %s", logname)
 
 global server
 global h_timer
@@ -79,12 +78,12 @@ def handler(signum, frame) -> None:
         None
     """
     signame = signal.Signals(signum).name
-    logging.error("%s\t| Signal handler called with signal %s (%d)", __name__, signame, signum)
-    logging.info("%s\t| Frame = %s", __name__, frame)
+    logger.error("%s\t| Signal handler called with signal %s (%d)", __name__, signame, signum)
+    logger.info("%s\t| Frame = %s", __name__, frame)
 
     # Handles a user input Ctrl + C
     if signame == "SIGINT":
-        logging.info("%s\t| User manually initiated shutdown using \"CTRL+C\"...", __name__)
+        logger.info("%s\t| User manually initiated shutdown using \"CTRL+C\"...", __name__)
         global FLAG
         FLAG = False
         h_timer.cancel()
@@ -98,17 +97,17 @@ class Server():
         self.backup_flag = False
 
         os.chdir(server_path)
-        logging.info('Starting server')
+        logger.info('Starting server')
         self.process = subprocess.Popen(executable, stdin=subprocess.PIPE)
-        logging.info("Server started.")
+        logger.info("Server started.")
 
     def __del__(self):
-        logging.warning("Deleting a Server object...")
+        logger.warning("Deleting a Server object...")
         # self.server_command("stop")
         # time.sleep(10)
-        logging.warning("Cancelling Process...")
+        logger.warning("Cancelling Process...")
         os.popen('TASKKILL /PID '+str(self.process.pid)+' /F')
-        logging.warning("Done!")
+        logger.warning("Done!")
 
     def server_command(self, cmd: str):
         """ Sends a string from the local python script to the server shell process
@@ -116,7 +115,7 @@ class Server():
         Args:
             cmd (str): The command string to be sent to the server
         """
-        logging.info("Writing server command: %s", cmd)
+        logger.info("Writing server command: %s", cmd)
         self.process.stdin.write(str.encode(f"{cmd}\n")) #just write the command to the input stream
         self.process.stdin.flush()
 
@@ -141,7 +140,7 @@ class Server():
         time.sleep(3)
 
         # Delete Expired Backups
-        logging.info('Deleting last file')
+        logger.info('Deleting last file')
         try:
             for filename in os.listdir(server_path):
                 # Extract useful info from path, first cut off directory, then cut off the filetype. Then separate info
@@ -155,8 +154,8 @@ class Server():
                 elif prev_backup_type == "Daily" and (cur_time - tstmp) > datetime.timedelta(days=DCAP):
                     os.remove(filename)
         except OSError as e:
-            logging.error(e)
-            logging.error("Error Removing Backup")
+            logger.error(e)
+            logger.error("Error Removing Backup")
 
         # Make the Backup
         backup_name = f"{self.server_name}_{backup_type}_{cur_time.strftime("%Y-%m-%dT%H-%m-%s")}"
@@ -173,11 +172,11 @@ class Server():
 
 
 def make_tarfile(output_filename, source_dir):
-    logging.info('Making tarfile')
+    logger.info('Making tarfile')
 
     def filter_function(tarinfo):
         if tarinfo.name != exclude_file:
-            logging.info(tarinfo.name,"ADDED")
+            logger.info(tarinfo.name,"ADDED")
             return tarinfo
 
     with tarfile.open(output_filename, "w:gz") as tar:
@@ -189,7 +188,7 @@ def make_tarfile(output_filename, source_dir):
 #     Returns:
 #         int: seconds left before next backup
 #     """
-#     logging.info('Calculating next time')
+#     logger.info('Calculating next time')
 #     if backup_type == "Daily":
 #         interval = datetime.timedelta(days=1)
 #     elif backup_type == "Hourly":
@@ -198,7 +197,7 @@ def make_tarfile(output_filename, source_dir):
 #     interval = datetime.timedelta(hours=1)
 #     current = datetime.datetime.today()
 #     next_time = current + interval
-#     logging.info("Next backup time is %s", next_time)
+#     logger.info("Next backup time is %s", next_time)
 #     return interval
 
 class BackupTimer(threading.Timer):
@@ -206,7 +205,7 @@ class BackupTimer(threading.Timer):
         while not self.finished.wait(self.interval):
             self.function(*self.args, **self.kwargs)
             next_time = datetime.datetime.now() + self.interval
-            logging.info("Next %s Backup time is at %s", *self.args[0], next_time)
+            logger.info("Next %s Backup time is at %s", *self.args[0], next_time)
 
 if __name__ == "__main__":
     global server
@@ -214,13 +213,13 @@ if __name__ == "__main__":
     time.sleep(45) # WAIT FOR IT TO START
     # TODO: Maybe check if process is alive? (EULA, crash)
 
-    logging.info('Starting backup timer')
+    logger.info('Starting backup timer')
     h_timer = BackupTimer(360, server.backup, args=("Hourly",))
     d_timer = BackupTimer(86400, server.backup, args=("Daily",))
-    logging.info("Timers Initialized")
+    logger.info("Timers Initialized")
     h_timer.start()
     d_timer.start()
-    logging.info('Timer started')
+    logger.info('Timer started')
 
     # Initialize Listener (for CTRL+C interrupts)
     signal.signal(signal.SIGINT, handler)
