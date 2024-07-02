766060049e400dcd940579b3b9a0d71adc47b43f
SeanDuffie <duffiesean@gmail.com>
2024-07-02 17:28:50 -0400

    Sort imports Remove file output logs Trim Extra from read_properties Added comments to mod_properties

--- Java/props.py
+++ Java/props.py
@@ -1,11 +1,11 @@
 """ _summary_
 """
+import configparser
+import datetime
 import logging
 import os
-import datetime
 
 import logFormat
-import configparser
 
 ### PATH SECTION ###
 DEFAULT_PATH = os.path.dirname(__file__)
@@ -13,7 +13,7 @@ ACTIVE_PATH = f"{DEFAULT_PATH}/active/"
 
 ### LOGGING SECTION ###
 logname = ACTIVE_PATH + '/' + 'MCSERVER.log'
-logFormat.format_logs(logger_name="MCLOG", file_name=logname)
+logFormat.format_logs(logger_name="MCLOG")
 logger = logging.getLogger("MCLOG")
 logger.info("Logname: %s", logname)
 
@@ -25,7 +25,7 @@ gitignore = [
 
 eula = [
     "#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://aka.ms/MinecraftEULA).\n",
-    datetime.datetime.now().astimezone().strftime("%a %b %d %H:%M:%S %Z %Y\n"),
+    datetime.datetime.now().astimezone().strftime("#%a %b %d %H:%M:%S %Z %Y\n"),
     "eula=TRUE\n"
 ]
 
@@ -48,16 +48,27 @@ def read_properties(path: str):
     for k, v in config['config'].items():
         print(k, v)
 
-        # try:
-        #     while True:
-        #         print(f.readline())
-        # except Exception as e:
-        #     print(e)
-        
-# def mod_properties():
-    
+def set_properties():
+    # difficulty: Default Game Difficulty. Easy by default, but I prefer Hard.
+    # enforce-secure-profile: Enables chat reporting. This is stupid and dumb.
+    # enforce-whitelist: Kicks all players not present on the whitelist
+    # TODO: force-gamemode: Survival is the goal, but maybe adventure is a better option?
+    #     gamemode: Same as above
+    # hardcore: ignore difficulty and set to spectator after death
+    # level-name: Name of the directory containing world files
+    # level-seed: seed of the world
+    # motd: Message displayed below the client server name. Add some personality!
+    # online-mode: Require Mojang authorized accounts. Default True, leave it on. Changing this will also delete/unlink all existing player data.
+    # previews-chat: Shows previews for features like chat color before sending. Default false, set true.
+    # simulation-distance: Maximum distance that client can update server or ticks can occur
+    # view-distance: world data sent to the client (server-side viewing distance). Default 10
+    # white-list: Enables whitelist (Not enforced yet, but can be added to)
+    pass
 
 if __name__ == "__main__":
-    # read_properties(f"{ACTIVE_PATH}server.properties")
-    write_file(gitignore, '.gitignore')
-    write_file(eula, 'eula.txt')
+    if not os.path.exists(f"{ACTIVE_PATH}.gitignore"):
+        write_file(gitignore, '.gitignore')
+    if not os.path.exists(f"{ACTIVE_PATH}eula.txt"):
+        write_file(eula, 'eula.txt')
+
+    read_properties(f"{ACTIVE_PATH}server.properties")
