c63030c440fdcd2bef6c799a0a2d8bdaef8dee5f
SeanDuffie <duffiesean@gmail.com>
2024-07-26 18:17:58 -0400

    FIXED: Crashing issue with loading nonexistant DiscordSRV/EssentialsX configs Load in and output yaml config files for discordsrv and essentialsx Ported documentation from the readme into the doxygen

--- Java/configs.py
+++ Java/configs.py
@@ -4,10 +4,9 @@ import configparser
 import datetime
 import logging
 import os
-# import yaml
-import ruamel.yaml
-import sys
 
+import yaml
+# import ruamel.yaml
 
 ### PATH SECTION ###
 DEFAULT_PATH = os.path.join(os.path.dirname(__file__), "Worlds/Server")
@@ -47,7 +46,7 @@ def gitignore(world_path: str = DEFAULT_PATH):
     # Output changes to file
     write_file(line_iterator=lines, filename=".gitignore", world_path=world_path)
 
-def properties(world_path: str):
+def properties(world_path: str = DEFAULT_PATH):
     """ Makes modifications to the server.properties file.
 
     Properties:
@@ -105,78 +104,138 @@ def properties(world_path: str):
     write_file(line_iterator=config_ls, filename="server.properties", world_path=world_path)
 
 def discordsrv(world_path: str = DEFAULT_PATH):
-    """ Makes modifications to the server.properties file.
+    """ Makes modifications to the DiscordSRV file.
 
-    Properties:
-        difficulty: Default Game Difficulty. Easy by default, but I prefer Hard.
-        enforce-secure-profile: Enables chat reporting. This is stupid and dumb.
-        enforce-whitelist: Kicks all players not present on the whitelist
-        TODO: force-gamemode: Survival is the goal, but maybe adventure is a better option?
-            gamemode: Same as above
-        hardcore: ignore difficulty and set to spectator after death
-        level-name: Name of the directory containing world files
-        level-seed: seed of the world
-        motd: Message displayed below the client server name. Add some personality!
-        online-mode: Require Mojang authorized accounts. Default True, leave it on. Changing this will also delete/unlink all existing player data.
-        previews-chat: Shows previews for features like chat color before sending. Default false, set true.
-        simulation-distance: Maximum distance that client can update server or ticks can occur
-        view-distance: world data sent to the client (server-side viewing distance). Default 10
-        white-list: Enables whitelist (Not enforced yet, but can be added to)
+    2. config.yml - This is the important file for setting up basic functionality of the bot.
+        1. BotToken: Paste the "Application ID" from the Discord Bot you created earlier
+        2. Channels: This allows you to filter which messages go to which channels, by default it
+        picks the top one. This creates a dictionary/json structure that specifies
+        {Type of Message}:{Channel ID}, some examples of categories are shown below:
+            1. global - player chat messages
+            2. status - server start/stop
+            3. awards - player achievements
+            4. deaths - player death messages
+            5. join - player join messages
+            6. leave - player leave messages
+            7. dynmap - TODO: Not sure what this is.
+            8. watchdog - TODO: performance?
+            9. broadcast - in-game players using the /broadcast command to message the Discord
+            10. link - account linking
+        3. DiscordInviteLink: The invite link to the server. Maybe make a separate link from default
+        invite to identify where they came from. Then make it a temporary membership for role
+        assignments.
+        4. MinecraftMentionSound - plays an in game sound to the player when they are mentioned in
+        Discord. Default True.
+        5. DiscordGameStatus: Contains a list of statuses that the Bot will cycle through, useful
+        for storing the Domain/IP
+        6. DiscordChatChannelDiscordToMinecraft: Sends Discord chat to in-game chat. Default True.
+        7. DiscordChatChannelMinecraftToDiscord: Sends in-game chat to Discord. IMO this creates
+        spam so I set it False. Default True.
+    3. linking.yml - Configuration options regarding linking your Minecraft account to a Discord
+    account.
+        1. Enabled: Set this to True to require linking. Default False.
+        2. Whitelisted players bypass check: If someone wants to join the server but doesn't want
+        to use Discord, this is a loophole. Default True.
+    4. messages.yml - Specify how you want various messages to look. E.g. Join messages, first join, leave, death.
+        1. Join: Nice on servers that are semi-active! Spam on heavily active servers. Awkward on dead servers. Default True.
+        2. First Join: Nice to show when new people join! Default True.
+        3. Leave: Best to keep it off unless you have a reason. Maybe someone wants to know if they missed other people? Or pranks? Default True.
+        4. Death: Good option for hazing opportunities. Can get spammy in some cases. Default True.
+        5. Achievement: Great for competition, or even cooperative celebration! Default True.
+        6. ChannelTopicUpdaterChatChannelTopicFormat: Formats the top bar of the text channel.
+            - Default: "%playercount%/%playermax% players online | %totalplayers% unique players ever joined | Server online for %uptimemins% minutes | Last update: %date%"
+            - Suggested: "%playercount%/%playermax% online | %tps% tps | %totalplayers% unique players | Online for %uptimemins% minutes | Last update: %date%"
+    5. synchronization.yml - Allows interactions between discord role and minecraft group
+        1. GroupRoleSynchronizationMinecraftIsAuthoritative: Does Minecraft set Discord roles? I say no. Default True.
+        2. GroupRoleSynchronizationOneWay: Can both sides set roles on the other? I say no. Default False.
+    6. voice.yml - Proximity Chat can be configured here!
+        1. Voice enabled: Enables Proximity Chat. Default False.
+        2. Voice Category: Create a category in the Discord, then copy the Category ID here.
+        3. Lobby Channel: Create a single voice channel in the above category and copy the Channel ID here.
+            - NOTE: This should be the only Channel in this Category.
+            - Anyone wanting to do voice proximity chat should join this call. In the Lobby, everyone will be muted, but when they approach each other, the bot will automatically create a new channel and move them to it.
+        4. Channels are visible: Set this to True for debugging purposes, but false improves immersion. Default False.
 
     Args:
         world_path (str, optional): Path to the world that is being run. Defaults to DEFAULT_PATH.
     """
     # Check that the server.properties file has already been generated
-    configfile = os.path.join(world_path, "plugins/DiscordSRV")
+    alertfile = os.path.join(world_path, "plugins\\DiscordSRV\\alerts.yml")
+    configfile = os.path.join(world_path, "plugins\\DiscordSRV\\config.yml")
+    linkingfile = os.path.join(world_path, "plugins\\DiscordSRV\\linking.yml")
+    messagesfile = os.path.join(world_path, "plugins\\DiscordSRV\\messages.yml")
+    syncfile = os.path.join(world_path, "plugins\\DiscordSRV\\synchronization.yml")
+    voicefile = os.path.join(world_path, "plugins\\DiscordSRV\\voice.yml")
+    if not os.path.isfile(alertfile):
+        logger.error("Alert File doesn't exist! Accept the EULA and launch the executable first!")
     if not os.path.isfile(configfile):
-        logger.error("File doesn't exist! Accept the EULA and launch the executable first!")
+        logger.error("Config File doesn't exist! Accept the EULA and launch the executable first!")
+    if not os.path.isfile(linkingfile):
+        logger.error("Linking File doesn't exist! Accept the EULA and launch the executable first!")
+    if not os.path.isfile(messagesfile):
+        logger.error("Messages File doesn't exist! Accept the EULA and launch the executable first!")
+    if not os.path.isfile(syncfile):
+        logger.error("Sync File doesn't exist! Accept the EULA and launch the executable first!")
+    if not os.path.isfile(voicefile):
+        logger.error("Voice File doesn't exist! Accept the EULA and launch the executable first!")
 
-    # Open DiscordSRV YAML File
-    yaml = ruamel.yaml.YAML()
-    config = yaml.load(configfile)
-    print(config)
+    # Open DiscordSRV YAML Files
+    # yaml = ruamel.yaml.YAML()
+    with open(file=alertfile, mode='r', encoding='utf_8') as f:
+        alert = yaml.safe_load(f)
+        print(alert)
+    with open(file=configfile, mode='r', encoding='utf_8') as f:
+        config = yaml.safe_load(f)
+        print(config)
+    with open(file=linkingfile, mode='r', encoding='utf_8') as f:
+        linking = yaml.safe_load(f)
+        print(linking)
+    with open(file=messagesfile, mode='r', encoding='utf_8') as f:
+        messages = yaml.safe_load(f)
+        print(messages)
+    with open(file=syncfile, mode='r', encoding='utf_8') as f:
+        sync = yaml.safe_load(f)
+        print(sync)
+    with open(file=voicefile, mode='r', encoding='utf_8') as f:
+        voice = yaml.safe_load(f)
+        print(voice)
 
     # Edit DiscordSRV Config
 
 
     # Output changes to file
     # write_file(line_iterator=config_ls, filename="server.properties", world_path=world_path)
+    with open(file=alertfile, mode='w+', encoding='utf_8') as f:
+        yaml.dump(alert, f)
     with open(file=configfile, mode='w+', encoding='utf_8') as f:
         yaml.dump(config, f)
+    with open(file=linkingfile, mode='w+', encoding='utf_8') as f:
+        yaml.dump(linking, f)
+    with open(file=messagesfile, mode='w+', encoding='utf_8') as f:
+        yaml.dump(messages, f)
+    with open(file=syncfile, mode='w+', encoding='utf_8') as f:
+        yaml.dump(sync, f)
+    with open(file=voicefile, mode='w+', encoding='utf_8') as f:
+        yaml.dump(voice, f)
 
 def essentialsx(world_path: str = DEFAULT_PATH):
-    """ Makes modifications to the server.properties file.
-
-    Properties:
-        difficulty: Default Game Difficulty. Easy by default, but I prefer Hard.
-        enforce-secure-profile: Enables chat reporting. This is stupid and dumb.
-        enforce-whitelist: Kicks all players not present on the whitelist
-        TODO: force-gamemode: Survival is the goal, but maybe adventure is a better option?
-            gamemode: Same as above
-        hardcore: ignore difficulty and set to spectator after death
-        level-name: Name of the directory containing world files
-        level-seed: seed of the world
-        motd: Message displayed below the client server name. Add some personality!
-        online-mode: Require Mojang authorized accounts. Default True, leave it on. Changing this will also delete/unlink all existing player data.
-        previews-chat: Shows previews for features like chat color before sending. Default false, set true.
-        simulation-distance: Maximum distance that client can update server or ticks can occur
-        view-distance: world data sent to the client (server-side viewing distance). Default 10
-        white-list: Enables whitelist (Not enforced yet, but can be added to)
+    """ Makes modifications to the EssentialsX configuration files.
 
     Args:
         world_path (str, optional): Path to the world that is being run. Defaults to DEFAULT_PATH.
     """
     # Check that the server.properties file has already been generated
-    configfile = os.path.join(world_path, "plugins/DiscordSRV")
+    configfile = os.path.join(world_path, "plugins\\Essentials\\config.yml")
     if not os.path.isfile(configfile):
         logger.error("File doesn't exist! Accept the EULA and launch the executable first!")
 
-    # Open DiscordSRV YAML File
-    yaml = ruamel.yaml.YAML()
-    config = yaml.load(configfile)
-    print(config)
+    # Open EssentialsX YAML File
+    # yaml = ruamel.yaml.YAML()
+    with open(file=configfile, mode='r', encoding='utf_8') as f:
+        config = yaml.safe_load(f)
+        print(config)
 
-    # Edit DiscordSRV Config
+    # Edit EssentialsX Config
 
 
     # Output changes to file
