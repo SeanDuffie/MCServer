deb24b717c683b0b1e9b4fee6032637e56941f7f
SeanDuffie <sean.duffie@tamu.edu>
2024-08-04 00:20:10 -0400

    Updated config.py to better handle missing .env file Fixed line too long linting errors

--- Java/configs.py
+++ Java/configs.py
@@ -6,7 +6,7 @@ import logging
 import os
 
 import yaml
-from dotenv import load_dotenv
+from dotenv import load_dotenv, set_key
 
 ### PATH SECTION ###
 DEFAULT_PATH = os.path.join(os.path.dirname(__file__), "Worlds/Server")
@@ -16,17 +16,10 @@ logger = logging.getLogger("MCLOG")
 
 ############ GET ENVIRONMENT VARIABLES ############
 RTDIR = os.path.dirname(__file__)
-
-load_dotenv(dotenv_path=f"{RTDIR}/.env")
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-PUBLIC_CHANNEL = os.getenv("PUBLIC_CHANNEL")
-ADMIN_CHANNEL = os.getenv("ADMIN_CHANNEL")
-LINK_CHANNEL = os.getenv("LINK_CHANNEL")
-CONSOLE_CHANNEL = os.getenv("CONSOLE_CHANNEL")
-VOICE_CATEGORY = os.getenv("VOICE_CATEGORY")
-LOBBY_CHANNEL = os.getenv("LOBBY_CHANNEL")
-INVITE_LINK = os.getenv("INVITE_LINK")
-SERVER_URL = os.getenv("SERVER_URL")
+if not load_dotenv(dotenv_path=f"{RTDIR}/.env"):
+    logger.warning("No Environment file detected! Generating a new one...")
+    with open(file=".env", mode="w", encoding="utf_8") as f:
+        pass  # Do nothing, just create the file
 ###################################################
 
 def eula(world_path: str = DEFAULT_PATH):
@@ -65,20 +58,22 @@ def properties(world_path: str = DEFAULT_PATH, server_type: str = "vanilla"):
     """ Makes modifications to the server.properties file.
 
     Properties:
-        difficulty: Default Game Difficulty. Easy by default, but I prefer Hard.
-        enforce-secure-profile: Enables chat reporting. This is stupid and dumb.
-        enforce-whitelist: Kicks all players not present on the whitelist
-        TODO: force-gamemode: Survival is the goal, but maybe adventure is a better option?
+        - difficulty: Default Game Difficulty. Easy by default, but I prefer Hard.
+        - enforce-secure-profile: Enables chat reporting. This is stupid and dumb.
+        - enforce-whitelist: Kicks all players not present on the whitelist
+        - force-gamemode: Survival is the goal, but maybe adventure is a better option?
             gamemode: Same as above
-        hardcore: ignore difficulty and set to spectator after death
-        level-name: Name of the directory containing world files
-        level-seed: seed of the world
-        motd: Message displayed below the client server name. Add some personality!
-        online-mode: Require Mojang authorized accounts. Default True, leave it on. Changing this will also delete/unlink all existing player data.
-        previews-chat: Shows previews for features like chat color before sending. Default false, set true.
-        simulation-distance: Maximum distance that client can update server or ticks can occur
-        view-distance: world data sent to the client (server-side viewing distance). Default 10
-        white-list: Enables whitelist (Enable if Vanilla, disable if using DiscordSRV)
+        - hardcore: ignore difficulty and set to spectator after death
+        - level-name: Name of the directory containing world files
+        - level-seed: seed of the world
+        - motd: Message displayed below the client server name. Add some personality!
+        - online-mode: Require Mojang authorized accounts. Default True, leave it on. Changing
+          this will also delete/unlink all existing player data.
+        - previews-chat: Shows previews for features like chat color before sending. Default False,
+            set True.
+        - simulation-distance: Maximum distance that client can update server or ticks can occur
+        - view-distance: world data sent to the client (server-side viewing distance). Default 10
+        - white-list: Enables whitelist (Enable if Vanilla, disable if using DiscordSRV)
 
     Args:
         world_path (str, optional): Path to the world that is being run. Defaults to DEFAULT_PATH.
@@ -154,30 +149,113 @@ def discordsrv(world_path: str = DEFAULT_PATH):
         1. Enabled: Set this to True to require linking. Default False.
         2. Whitelisted players bypass check: If someone wants to join the server but doesn't want
         to use Discord, this is a loophole. Default True.
-    4. messages.yml - Specify how you want various messages to look. E.g. Join messages, first join, leave, death.
-        1. Join: Nice on servers that are semi-active! Spam on heavily active servers. Awkward on dead servers. Default True.
+    4. messages.yml - Specify how you want various messages to look. E.g. Join messages, first
+       join, leave, death.
+        1. Join: Nice on servers that are semi-active! Spam on heavily active servers. Awkward on
+           dead servers. Default True.
         2. First Join: Nice to show when new people join! Default True.
-        3. Leave: Best to keep it off unless you have a reason. Maybe someone wants to know if they missed other people? Or pranks? Default True.
+        3. Leave: Best to keep it off unless you have a reason. Maybe someone wants to know if they
+           missed other people? Or pranks? Default True.
         4. Death: Good option for hazing opportunities. Can get spammy in some cases. Default True.
         5. Achievement: Great for competition, or even cooperative celebration! Default True.
         6. ChannelTopicUpdaterChatChannelTopicFormat: Formats the top bar of the text channel.
-            - Default: "%playercount%/%playermax% players online | %totalplayers% unique players ever joined | Server online for %uptimemins% minutes | Last update: %date%"
-            - Suggested: "%playercount%/%playermax% online | %tps% tps | %totalplayers% unique players | Online for %uptimemins% minutes | Last update: %date%"
+            - Default: "%playercount%/%playermax% players online | %totalplayers% unique players
+                        ever joined | Server online for %uptimemins% minutes | Last update: %date%"
+            - Suggested: "%playercount%/%playermax% online | %tps% tps | %totalplayers% unique
+                          players | Online for %uptimemins% minutes | Last update: %date%"
     5. synchronization.yml - Allows interactions between discord role and minecraft group
-        1. GroupRoleSynchronizationMinecraftIsAuthoritative: Does Minecraft set Discord roles? I say no. Default True.
-        2. GroupRoleSynchronizationOneWay: Can both sides set roles on the other? I say no. Default False.
+        1. GroupRoleSynchronizationMinecraftIsAuthoritative: Does Minecraft set Discord roles? I
+           say no. Default True.
+        2. GroupRoleSynchronizationOneWay: Can both sides set roles on the other? I say no.
+           Default False.
     6. voice.yml - Proximity Chat can be configured here!
         1. Voice enabled: Enables Proximity Chat. Default False.
         2. Voice Category: Create a category in the Discord, then copy the Category ID here.
-        3. Lobby Channel: Create a single voice channel in the above category and copy the Channel ID here.
+        3. Lobby Channel: Create a single voice channel in the above category and copy the Channel
+           ID here.
             - NOTE: This should be the only Channel in this Category.
-            - Anyone wanting to do voice proximity chat should join this call. In the Lobby, everyone will be muted, but when they approach each other, the bot will automatically create a new channel and move them to it.
-        4. Channels are visible: Set this to True for debugging purposes, but false improves immersion. Default False.
+            - Anyone wanting to do voice proximity chat should join this call. In the Lobby,
+              everyone will be muted, but when they approach each other, the bot will automatically
+              create a new channel and move them to it.
+        4. Channels are visible: Set this to True for debugging purposes, but false improves
+           immersion. Default False.
 
     Args:
         world_path (str, optional): Path to the world that is being run. Defaults to DEFAULT_PATH.
     """
     # TODO: Add error handling or validation for environment file
+    discord_token = os.getenv("DISCORD_TOKEN")
+    if discord_token is None:
+        discord_token = input("Enter the Discord Token: ")
+        set_key(
+            dotenv_path=f"{RTDIR}/.env",
+            key_to_set="PUBLIC_CHANNEL",
+            value_to_set=discord_token
+        )
+    public_channel = os.getenv("PUBLIC_CHANNEL")
+    if public_channel is None:
+        public_channel = input("Enter the Channel ID of the Public Discord Channel: ")
+        set_key(
+            dotenv_path=f"{RTDIR}/.env",
+            key_to_set="PUBLIC_CHANNEL",
+            value_to_set=public_channel
+        )
+    admin_channel = os.getenv("ADMIN_CHANNEL")
+    if admin_channel is None:
+        admin_channel = input("Enter the Channel ID of the Admin Discord Channel: ")
+        set_key(
+            dotenv_path=f"{RTDIR}/.env",
+            key_to_set="PUBLIC_CHANNEL",
+            value_to_set=admin_channel
+        )
+    link_channel = os.getenv("LINK_CHANNEL")
+    if link_channel is None:
+        link_channel = input("Enter the Channel ID of the Account Linking Channel: ")
+        set_key(
+            dotenv_path=f"{RTDIR}/.env",
+            key_to_set="PUBLIC_CHANNEL",
+            value_to_set=link_channel
+        )
+    console_channel = os.getenv("CONSOLE_CHANNEL")
+    if console_channel is None:
+        console_channel = input("Enter the Channel ID of the Server Console Channel: ")
+        set_key(
+            dotenv_path=f"{RTDIR}/.env",
+            key_to_set="PUBLIC_CHANNEL",
+            value_to_set=public_channel
+        )
+    voice_category = os.getenv("VOICE_CATEGORY")
+    if voice_category is None:
+        voice_category = input("Enter the Category ID of the proximity voice category: ")
+        set_key(
+            dotenv_path=f"{RTDIR}/.env",
+            key_to_set="PUBLIC_CHANNEL",
+            value_to_set=public_channel
+        )
+    lobby_channel = os.getenv("LOBBY_CHANNEL")
+    if lobby_channel is None:
+        lobby_channel = input("Enter the Voice Channel ID of the Proximity Lobby: ")
+        set_key(
+            dotenv_path=f"{RTDIR}/.env",
+            key_to_set="PUBLIC_CHANNEL",
+            value_to_set=public_channel
+        )
+    invite_link = os.getenv("INVITE_LINK")
+    if invite_link is None:
+        invite_link = input("Enter the Discord server invite link: ")
+        set_key(
+            dotenv_path=f"{RTDIR}/.env",
+            key_to_set="PUBLIC_CHANNEL",
+            value_to_set=public_channel
+        )
+    server_url = os.getenv("SERVER_URL")
+    if server_url is None:
+        server_url = input("Enter the Minecraft server URL: ")
+        set_key(
+            dotenv_path=f"{RTDIR}/.env",
+            key_to_set="PUBLIC_CHANNEL",
+            value_to_set=public_channel
+        )
 
     # Check that the server.properties file has already been generated
     alertfile = os.path.join(world_path, "plugins\\DiscordSRV\\alerts.yml")
@@ -187,17 +265,17 @@ def discordsrv(world_path: str = DEFAULT_PATH):
     syncfile = os.path.join(world_path, "plugins\\DiscordSRV\\synchronization.yml")
     voicefile = os.path.join(world_path, "plugins\\DiscordSRV\\voice.yml")
     if not os.path.isfile(alertfile):
-        logger.error("Alert File doesn't exist! Accept the EULA and launch the executable first!")
+        logger.error("Alert File doesn't exist! Accept the EULA and launch executable first!")
     if not os.path.isfile(configfile):
-        logger.error("Config File doesn't exist! Accept the EULA and launch the executable first!")
+        logger.error("Config File doesn't exist! Accept the EULA and launch executable first!")
     if not os.path.isfile(linkingfile):
-        logger.error("Linking File doesn't exist! Accept the EULA and launch the executable first!")
+        logger.error("Linking File doesn't exist! Accept the EULA and launch executable first!")
     if not os.path.isfile(messagesfile):
-        logger.error("Messages File doesn't exist! Accept the EULA and launch the executable first!")
+        logger.error("Messages File doesn't exist! Accept the EULA and launch executable first!")
     if not os.path.isfile(syncfile):
-        logger.error("Sync File doesn't exist! Accept the EULA and launch the executable first!")
+        logger.error("Sync File doesn't exist! Accept the EULA and launch executable first!")
     if not os.path.isfile(voicefile):
-        logger.error("Voice File doesn't exist! Accept the EULA and launch the executable first!")
+        logger.error("Voice File doesn't exist! Accept the EULA and launch executable first!")
 
     # Read and Edit DiscordSRV YAML Files
     # yaml = ruamel.yaml.YAML()
@@ -205,22 +283,22 @@ def discordsrv(world_path: str = DEFAULT_PATH):
     #     alert = yaml.safe_load(f)
     with open(file=configfile, mode='r', encoding='utf_8') as f:
         config = yaml.safe_load(f)
-        config["BotToken"] = DISCORD_TOKEN
+        config["BotToken"] = discord_token
         config["Channels"] = {
-            "global": ADMIN_CHANNEL,
-            "awards": PUBLIC_CHANNEL,
-            "deaths": PUBLIC_CHANNEL,
-            "join": PUBLIC_CHANNEL,
-            "broadcasts": PUBLIC_CHANNEL,
-            "link": LINK_CHANNEL
+            "global": admin_channel,
+            "awards": public_channel,
+            "deaths": public_channel,
+            "join": public_channel,
+            "broadcasts": public_channel,
+            "link": link_channel
         }
-        config["DiscordConsoleChannelId"] = CONSOLE_CHANNEL
-        config["DiscordInviteLink"] = INVITE_LINK
-        config["DiscordGameStatus"] = [SERVER_URL]
+        config["DiscordConsoleChannelId"] = console_channel
+        config["DiscordInviteLink"] = invite_link
+        config["DiscordGameStatus"] = [server_url]
         config["DiscordCannedResponses"] = {
-            "!ip": SERVER_URL,
-            "!site": SERVER_URL,
-            "!discord": INVITE_LINK
+            "!ip": server_url,
+            "!site": server_url,
+            "!discord": invite_link
         }
     with open(file=linkingfile, mode='r', encoding='utf_8') as f:
         linking = yaml.safe_load(f)
@@ -237,8 +315,8 @@ def discordsrv(world_path: str = DEFAULT_PATH):
     with open(file=voicefile, mode='r', encoding='utf_8') as f:
         voice = yaml.safe_load(f)
         voice["Voice enabled"] = True
-        voice["Voice category"] = VOICE_CATEGORY
-        voice["Lobby channel"] = LOBBY_CHANNEL
+        voice["Voice category"] = voice_category
+        voice["Lobby channel"] = lobby_channel
         # Do I want this???
         voice["Network"]["Channels are visible"] = False
 
