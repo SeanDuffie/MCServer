36539213b016c6988f34be2cc2f5ff661c18853b
SeanDuffie <duffiesean@gmail.com>
2025-06-16 01:56:34 -0400

    Added ability to automatically detect Minecraft version, then determine corresponding Java version. If proper Java version is not installed, provide a link to the download page for the desired Java version.

--- Java/server.py
+++ Java/server.py
@@ -8,11 +8,93 @@ import os
 import subprocess
 import sys
 import time
+from tkinter import filedialog
 from typing import Literal
 
 from pipeline import Pipeline
 
 logger = logging.getLogger("MCServer")
+
+def get_mc_ver(filename: str):
+    """ Get the current MC version from the Jar file name.
+
+    Args:
+        filename (str): Filename of the Minecraft server executable.
+
+    Returns:
+        float: Number to represent the version.
+    """
+    # Get hyphens that outline the version
+    first = filename.index("-")
+    last = filename.index("-", first+1)
+    # Cut off the "1." (major version) to convert to float
+    version = float(filename[first+1:last][2:])
+    return version
+
+def get_java(version: float):
+    """ Gets the path to the proper version of Java to use for the current Minecraft version.
+
+    Args:
+        version (float): Current version of minecraft.
+
+    Returns:
+        str: Path to the corresponding version of Java.
+    """
+    # Select Java Version
+    java_path = r"C:\Program Files\Java"
+
+    if version == 0:
+        java_path = "java"
+    if version <= 16.5:
+        #Java 8
+        if os.path.isdir(r"C:\Program Files\Java\jdk-1.8\bin"):
+            java_path = r"C:\Program Files\Java\jdk-1.8\bin\java.exe"
+        else:
+            logging.error("Java 8 is not detected! Please make sure it is installed")
+            logging.error("properly, in the correct location, and with the correct name.")
+            logging.error("http://oracle.com/java/technologies/downloads/#java8-windows")
+    elif version < 18:
+        #Java 16
+        if os.path.isdir(r"C:\Program Files\Java\jdk-16.0.2\bin"):
+            java_path = r"C:\Program Files\Java\jdk-16.0.2\bin\java.exe"
+        elif os.path.isdir(r"C:\Program Files\Java\jdk-16.0.1\bin"):
+            java_path = r"C:\Program Files\Java\jdk-16.0.1\bin\java.exe"
+        elif os.path.isdir(r"C:\Program Files\Java\jdk-16\bin"):
+            java_path = r"C:\Program Files\Java\jdk-16\bin\java.exe"
+        else:
+            logging.error("Java 16 is not detected! Please make sure it is installed")
+            logging.error("properly, in the correct location, and with the correct name.")
+            logging.error("https://jdk.java.net/archive/")
+            sys.exit(1)
+    elif version <= 20.4:
+        #Java 17
+        if os.path.isdir(r"C:\Program Files\Java\jdk-17\bin"):
+            java_path = r"C:\Program Files\Java\jdk-17\bin\java.exe"
+        elif os.path.isdir(r"C:\Program Files\Java\jdk-17.0.2\bin"):
+            java_path = r"C:\Program Files\Java\jdk-17.0.2\bin\java.exe"
+        elif os.path.isdir(r"C:\Program Files\Java\jdk-17.0.1\bin"):
+            java_path = r"C:\Program Files\Java\jdk-17.0.1\bin\java.exe"
+        else:
+            logging.error("Java 17 is not detected! Please make sure it is installed")
+            logging.error("properly, in the correct location, and with the correct name.")
+            logging.error("https://www.oracle.com/java/technologies/downloads/#java17-windows")
+            sys.exit(1)
+    else:
+        #Java 21 https://www.oracle.com/java/technologies/downloads/#jdk21-windows
+        # Java 24
+        if os.path.isdir(r"C:\Program Files\Java\jdk-24.0.1\bin"):
+            java_path = r"C:\Program Files\Java\jdk-24.0.1\bin\java.exe"
+        elif os.path.isdir(r"C:\Program Files\Java\jdk-22.0.1\bin"):
+            java_path = r"C:\Program Files\Java\jdk-22.0.1\bin\java.exe"
+        elif os.path.isdir(r"C:\Program Files\Java\jdk-22\bin"):
+            java_path = r"C:\Program Files\Java\jdk-22\bin\java.exe"
+        else:
+            logging.error("Java 24 is not detected! Please make sure it is installed")
+            logging.error("properly, in the correct location, and with the correct name.")
+            logging.error("https://jdk.java.net/24/")
+            sys.exit(1)
+
+    return java_path
 
 class Server():
     """ The Server object provides a wrapper for functions related to the minecraft server jar
@@ -28,26 +110,33 @@ class Server():
         self.backup_flag = False
 
         os.chdir(self.server_path)
-        logger.info('Starting server')
+        logger.info("Starting server")
         self.run(ram=ram)
 
         logger.info("Server started.")
         self.p = Pipeline(src_dir=self.server_path, zip_dir=zip_dir)
 
-    def run(self, ram: int = 8):
+    def run(self, version: float = 0, ram: int = 8):
         """ Called when the server is launched.
 
             Launches and attaches the server process to self.process variable.
             This is called in __init__ and must finish before any interactions with the server can
             occur.
-            
+
         Args:
+            java (float): What version of Java to use in the command line. By default, the java
+                command is set by "JAVA_HOME" in the Environment Variables. For greater control,
+                you can pass the direct path to the "java.exe" in the bin folder of the desired
+                Java version.
             ram (int): Amount of ram in Gigabytes to allocate for the server.
         """
         executable = ""
+
         for filename in os.listdir(self.server_path):
             if filename.endswith(".jar"):
-                executable = f"java -Xmx{ram * 1024}m -jar \"{filename}\" nogui"
+                version = get_mc_ver(filename)
+                java_path = get_java(version)
+                executable = f"{java_path} -Xmx{ram * 1024}m -jar \"{filename}\" nogui"
                 break
 
         if executable == "":
@@ -135,3 +224,6 @@ class Server():
         else:
             logger.error("Something went wrong with the restore! Attempting to revert changes...")
         return check
+
+if __name__ == "__main__":
+    print(get_mc_ver("paper-1.17.1-388.jar"))
